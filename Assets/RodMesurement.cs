0a18beff29c33a95c3f0569464fa78e0e81d4eb7
Karhal <mar.fucik@gmail.com>
2020-03-22 21:24:47 +0100

    Add Mesaure by Rod

---
+++ Assets/RodMesurement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RodMesurement : MonoBehaviour
+{
+    Vector2 startMousePos = new Vector2();
+
+    public float rodMesurement = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = MouseWorldPosition();
+            startMousePos = mousePos;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = MouseWorldPosition();
+            Vector3 cameraPos = Camera.main.transform.position;
+
+            Vector2 direction = mousePos - startMousePos;
+
+            float snapedYPos = Mathf.Round((cameraPos.y - direction.y) * 100) / 100;
+            print(snapedYPos);
+
+            Camera.main.transform.position = new Vector3(
+                0f,
+                snapedYPos,
+                cameraPos.z
+            );
+        }
+
+        rodMesurement = Camera.main.transform.position.y;
+
+    }
+
+    private static Vector2 MouseWorldPosition()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(clickPos);
+        return mousePos;
+    }
+}
---
+++ Assets/Scripts/CanvasDirector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasDirector : MonoBehaviour
+{
+    [SerializeField] Canvas mainCanvas = null;
+    [SerializeField] Canvas mesureCanvas = null;
+
+    InputField inputField = null;
+
+    public void SwitchToRodMesurement(InputField inputField)
+    {
+        CanvasVisibility(mainCanvas, false);
+        CanvasVisibility(mesureCanvas, true);
+        this.inputField = inputField;
+    }
+
+    public void SwitchOffRodMesurement()
+    {
+        CanvasVisibility(mainCanvas, true);
+        CanvasVisibility(mesureCanvas, false);
+        inputField.text = Camera.main.transform.position.y.ToString("##0.00#");
+    }
+
+    private void CanvasVisibility(Canvas canvas, bool state)
+    {
+        canvas.enabled = state;
+    }
+}
